eb578a9033d66aeb1579f5d88cddddfe4f601307
JJHedges <jordanjameshedges3@gmail.com>
2016-02-08 19:34:56 +0800

    Added comments

--- client.cpp
+++ client.cpp
@@ -3,12 +3,21 @@
 
 int main(int argc, char** argv)
 {
+	
 	if(argc != 3)
 	{
 		std::cout << "Usage is 'ip address' 'port number'" << std::endl;
 	}
 	else
 	{
-		servConnection::servConnection servConn();
+		char *message;
+		servConnection::servConnection servConn(argv[1], argv[2]);
+		do
+		{
+			std::cin >> message;
+			servConn.writeServer(message);
+			servConn.readServer();
+			servConn.readServer();
+		}while(strcmp(message, "exit") != 0);
 	}
 }
--- servConnection.hpp
+++ servConnection.hpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+
+/*
+ * Class that manages connections to the server by a client.
+ */
 
 namespace servConnection
 {
@@ -16,13 +21,15 @@ namespace servConnection
             
         private:
         	int sockfd;
+        	//Specifications for the type of socket that we want to set up
         	struct addrinfo sockSpecs;
+        	//Results of getaddrinfo go here once called
         	struct addrinfo *infoResults;        			 	
     };
     
     servConnection::~servConnection()
     {
-        	
+    	shutdown(sockfd, 0);
     }
         	
     servConnection::servConnection(const char* ipAddr, const char* servPort)
@@ -34,34 +41,24 @@ namespace servConnection
     
     	try
 		{
+			//Get address info of server on ip ipAddr and port servPort
 			int status = getaddrinfo(ipAddr, servPort, &sockSpecs, &infoResults);
 			if(status == -1)
 			{
 				throw(strerror(status));
 			}
-		}
-		catch (char *msg)
-		{
-			std::cerr << msg;
-		}
 	
-		try
-		{
+			//Create socket, resulting identifier goes in sockfd
 			sockfd = socket(infoResults->ai_family, infoResults->ai_socktype,
 							infoResults->ai_protocol);
 			if(sockfd == -1)
 			{
 				throw(strerror(errno));
 			}
-		}
-		catch (char *msg)
-		{
-			std::cerr << msg;
-		}
-		
-		try
-		{
-			int status = connect(sockfd, infoResults->ai_addr, sizeof(infoResults->ai_addr));
+
+
+			//Try to connect the created socket to server, result goes in status
+			status = connect(sockfd, infoResults->ai_addr, sizeof(infoResults->ai_addr));
 			if(status == -1)
 			{
 				throw(strerror(errno));
@@ -75,11 +72,43 @@ namespace servConnection
 	
 	void servConnection::writeServer(char* message)
 	{
-		
+		int status;
+		try
+		{
+			//Writes to server socket, throws exception on error
+			status = write(sockfd, message, 255);
+			if(status == -1)
+			{
+				throw(strerror(errno));
+			}
+		}
+		catch(char *msg)
+		{
+			std::cerr << msg;
+		}
 	}
 	
 	void servConnection::readServer()
 	{
-		
+		fd_set readSet;
+		int result, status;
+		try
+		{
+			//Checks if server has sent data
+			result = select(sockfd + 1, &readSet, NULL, NULL, NULL);
+			if(result == -1)
+			{
+				throw(strerror(errno));
+			}
+			else if(result == 1)
+			{
+				//If server has sent data then read it and output it
+				
+			}
+		}
+		catch(char *msg)
+		{
+			std::cerr << msg;
+		}				
 	}
 }
